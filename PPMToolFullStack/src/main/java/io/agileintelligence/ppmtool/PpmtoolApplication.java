66597688df2c5b1c88f61055a266ad8967b6d419
Yu Li <yli14@wpi.edu>
2019-08-24 15:39:30 -0400

    encrypted password in application properties

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/PpmtoolApplication.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/PpmtoolApplication.java
@@ -1,11 +1,13 @@
 package io.agileintelligence.ppmtool;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class PpmtoolApplication {
 
     @Bean
